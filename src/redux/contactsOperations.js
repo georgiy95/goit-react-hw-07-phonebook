737b92182e03c5e3f229bc602fa6fff262fda3af
Georgiy95 <urijpetrovskij@gmail.com>
2023-10-19 12:52:05 +0300

    Update contactsOperations.js

--- src/redux/contactsOperations.js
+++ src/redux/contactsOperations.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import * as contactsApi from '../services/contacts-api';
 
 axios.defaults.baseURL = 'https://64fc97af605a026163aea6df.mockapi.io';
 
@@ -9,6 +10,7 @@ export const fetchContacts = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
       const response = await axios.get('/contacts');
+      
       return response.data;
     } catch (evt) {
       return thunkAPI.rejectWithValue(evt.message);
@@ -17,6 +19,18 @@ export const fetchContacts = createAsyncThunk(
 );
 
 export const addContact = createAsyncThunk(
+  '/contacts/addContact',
+  async (contact, { rejectWithValue }) => {
+      try {
+          const addedContact = await contactsApi.addContacts(contact);
+          return addedContact;
+      } catch (error) {
+          return rejectWithValue(error);
+      }
+  }
+);
+
+export const createContact = createAsyncThunk(
   'contacts/postContact',
   async (arg, thunkAPI) => {
     try {
