fd046c0b29d65dba524d11cf51cf795e3c29b7e0
Georgiy95 <urijpetrovskij@gmail.com>
2023-10-19 20:34:11 +0300

    ф

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import { contactsReducer } from './contacts/contactsSlice';
-// import contactsReducer from './contactsReducer';
 import { filterSlice } from './filterSlice';
 
 export const store = configureStore({
     reducer: {
-        // contacts: contactsReducer,
         filter: filterSlice.reducer,
     },
 });
